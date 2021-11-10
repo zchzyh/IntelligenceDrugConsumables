9b53e0db27b906fa00596e838f53c345099ada12
zchzyh <zchzyh@yeah.net>
2021-11-10 10:19:42 +0800

    新增两组页面

---
+++ LeaRun.Application/LeaRun.Application.Web/Areas/BusinessManage/Controllers/BusinessDataController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LeaRun.Application.Web.Areas.BusinessManage
+{
+    public class BusinessDataController : Controller
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult PatientData()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult PacsData()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult LisData()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult SensingData()
+        {
+            return View();
+        }
+    }
+}
---
+++ LeaRun.Application/LeaRun.Application.Web/Areas/BusinessManage/Controllers/BusinessSystemController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LeaRun.Application.Web.Areas.BusinessManage
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class BusinessSystemController : Controller
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult SystemManage()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult EditionManage()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult SystemParameterManage()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult SystemConfigureManage()
+        {
+            return View();
+        }
+
+    }
+}
